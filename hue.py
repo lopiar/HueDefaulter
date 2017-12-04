b3e24a2cf9c4eefd4a08e7d83fad4a8f02ef960e
Vivan Jayant <vivan@vivan.me>
2017-12-04 15:45:32 +0000

    Added multiple logic options

--- hue.py
+++ hue.py
@@ -2,9 +2,11 @@
 
 from phue import Bridge
 import time
+import json
 
-ideal_temp = 275
-bridge_ip = '192.168.1.64' # Enter bridge IP here.
+default_temp = 366
+ideal_temp = 275  # Enter the desired colour temp here.
+bridge_ip = '192.168.1.64'  # Enter bridge IP here.
 
 b = Bridge(bridge_ip)
 
@@ -14,24 +16,36 @@ b.connect()
 lights = b.get_light_objects()
 try:
     while True:
+        # Version 1: This checks all the lights individually and then fixes them all in one go. It is joint second fastest and joint worst performance.
         # lights_to_change = []
         # for light in lights:
-        #     if light.colortemp == 366:
+        #     if light.colortemp == default_temp:
         #         lights_to_change.append(light)
         # if lights_to_change:
-        #     print('Changing these lights: ',lights_to_change)
+        #     print('Fiding: ',lights_to_change)
         #     for l in lights_to_change:
-        #         l.colortemp = 275
-        #         l.on = False
+        #         l.colortemp = ideal_temp
 
+        # Version 2: This gets the data for all the lights in one go, then checks which need to be fixed and fixes them in one go. It is the slowest but the best for performance.
+        # lights = json.dumps(b.get_api().get('lights'))
+        # lights = json.loads(lights)
+        # lights_to_change = [int(d) for d in lights if lights[d]['state']['ct'] == default_temp]
+        # if lights_to_change:
+        #     b.set_light(lights_to_change, 'ct', ideal_temp)
+        #     print('Fixing: ',lights_to_change)
+
+        # Version 3: This checks each light invidiually then fixes the light before moving to the next one. It is joint second fastest and joint worst performance.
         for light in lights:
-            if light.colortemp == 366:
+            if light.colortemp == default_temp:
                 light.colortemp = ideal_temp
-                print('Fixed light: ', light.light_id)
+                print('Fixing: ', light.light_id)
 
-        time.sleep(1)
+        # Version 4: This doesn't check the lights at all, it just tells them all to go to the right temp over and over. It is the fastest and second best performance, but it means that all bulbs will be set to the same hue.
+        # for light in lights:
+        #     if light.colortemp == default_temp:
+        #         light.colortemp = ideal_temp
+        #         print('Fixing: ', light.light_id)
+
+        time.sleep(1)  # Change this to whatever you like. Higher number improves performance but slows down the fixing
 except KeyboardInterrupt:
     print('interrupted!')
-
-
-
